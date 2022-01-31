e3c0da43cc549b350933b0dfc5e867f3744e0e29
IanAmbrose <ianambrose@pm.me>
2022-01-31 18:56:23 +0000

    init

---
+++ Python - OOP start/OOP - CSDOJO.py
@@ -0,0 +1,38 @@
+class Robot:
+    def __init__(self,name,color,weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+    
+    def introduceSelf(self):
+        print(f'Hello my name is {self.name}')
+
+class Person:
+    def __init__(self, name, personality, isSitting):
+        self.name = name
+        self.personality = personality
+        self.isSitting = isSitting
+
+    def sit_down(self):
+        self.isSitting = True
+
+    def stand_up(self):
+        self.isSitting = False
+
+
+
+
+r1 = Robot('Ian', 'red', 50)
+r2 = Robot('John', 'silver', 70)
+
+r1.introduceSelf()
+
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "talkative", True)
+
+
+p1.robot_owned = r2
+p2.robot_owned = r1
+#p1 owns r2
+
+p1.robot_owned.introduceSelf()
