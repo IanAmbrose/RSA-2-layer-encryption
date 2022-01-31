afddab0ed17e54065ca24f52282ece46fd9dd4d7
IanAmbrose <ianambrose@pm.me>
2022-01-31 22:29:37 +0000

    BankingProject

--- Python - OOP/BankingPractice.py
+++ Python - OOP/BankingPractice.py
@@ -6,7 +6,7 @@
 #Stroes details about the amount
 #Allows for deposits, withdraw, view_balance
 
-
+#Parent Class
 class User():
     def __init__(self, name, gender, age):
         self.name = name
@@ -14,31 +14,38 @@ class User():
         self.age = age
 
     def show_user_details(self):
-        print("Personal Details")
-        print(f'''User: {self.name}
-                  Gender: {self.gender}
-                  Age: {self.age}
-             ''')
+        print("Personal Details\n")
+        print(f'User:{self.name}\nGender: {self.gender}\nAge: {self.age} ')
 
-
+#Child Class
 class Bank(User):
 
-    def __init__(self, first, last, pin):
-        pass
-    def account_balance(self):
-        pass
-
-    def deposit(self):
-        pass
-
-    def withdraw(self):
-        pass
+    def __init__(self, name, gender, age):
+        super().__init__(name, gender, age)
+        self.balance = 0
 
     def view_balance(self):
-        pass
+        self.show_user_details()
+        print(f'Balance: £{self.balance}')
 
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + amount
+        print(f'Account balance has been updated £{self.balance}')
 
+    def withdraw(self, amount): 
+        self.amount = amount
+        if self.amount > self.balance:
+            print(f'Insufficient Funds | Balance available £{self.balance}')
+        else:
+            self.balance = self.balance - amount
+            print(f'Account balance has been updated £{self.balance}')
 
 p1 = User('Ian Ambrose', 'Male', 21)
+b1 = Bank('Ian AmbroseBank', 'Male', 21)
+
 
 p1.show_user_details()
+b1.deposit(500)
+b1.withdraw(150)
+b1.view_balance()
